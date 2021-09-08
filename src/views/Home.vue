2071a6cc62b5f8c648eb42f223b7cccb487b6056
koushik k <koushik@billionlives.in>
2021-09-08 17:19:28 +0530

    table ui & json file created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,81 @@
 <template>
-  <div class="pa-0 ma-0">
-    <v-container fluid class="ma-0 pa-0">
-      <v-card style="height:100vh">
-        <v-row style="height:80px">
-          <v-col cols="12" class="d-flex">
-            <v-col cols="2"><h4 class="pt-1">Users</h4></v-col>
+  <div class="home-div grey lighten-3">
+    <v-card style="height:90vh" class="pa-0 ma-0">
+        <v-row>
+          <v-col cols="12" class="d-inline-flex" style="height:70px">
+            <h4 class="pt-1 ml-4">Users</h4>
             <v-spacer></v-spacer>
-            <v-col cols="2"><v-btn color="primary" dense class="">Create User</v-btn></v-col>
+            <v-btn dense color="primary" class="mr-4">Create User</v-btn>
           </v-col>
         </v-row>
         <v-divider></v-divider>
+        <v-row class="d-flex justify-center">
+          <v-col cols="11">
+            <table class="mt-6">
+              <tr>
+                <th>Name</th>
+                <th>SurName</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Activate</th>
+                <th>Action</th>
+              </tr>
+              <tr v-for="user in users" :key="user.id">
+                <td>{{user.Name}}</td>
+                <td>{{user.Surname}}</td>
+                <td>{{user.Email}}</td>
+                <td>{{user.Phone}}</td>
+                <td>{{user.Status}}</td>
+                <td><v-btn dense color="success">Edit</v-btn><v-btn dense color="error" class="ml-2">Delete</v-btn></td>
+              </tr>
+            </table>
+          </v-col>
+        </v-row>
       </v-card>
-    </v-container>
   </div>
 </template>
 
 <script>
-
-
+import userData from "../users.json";
 export default {
   name: "Home",
+
+  data(){
+    return{
+      users: userData,
+    }
+  },
 
   components: {
     // HelloWorld,
   },
 };
 </script>
+
+<style scoped>
+
+.home-div{
+  padding: 40px 20px;
+}
+
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+  text-align: left;
+}
+
+tr,th {
+  border-top: 2px solid #ddd;
+  border-bottom: 2px solid #ddd;
+  /* padding: 10px 5px; */
+}
+
+td,th{
+  padding: 10px 20px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
