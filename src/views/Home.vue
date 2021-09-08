dc60b6c3b8188faef0877ef892ccc78d11a0a859
koushik k <koushik@billionlives.in>
2021-09-08 16:19:51 +0530

    code clean up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="pa-0 ma-0">
+    <v-container fluid class="ma-0 pa-0">
+      <v-card style="height:100vh">
+        <v-row style="height:80px">
+          <v-col cols="12" class="d-flex">
+            <v-col cols="2"><h4 class="pt-1">Users</h4></v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="2"><v-btn color="primary" dense class="">Create User</v-btn></v-col>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "Home",
+
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
