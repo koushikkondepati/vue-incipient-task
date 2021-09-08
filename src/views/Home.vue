9be0133d7c606d417bf8784ff769097f172908db
koushik k <koushik@billionlives.in>
2021-09-08 20:22:13 +0530

    delete btn added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,13 @@
           <v-col cols="12" class="d-inline-flex" style="height:70px">
             <h4 class="pt-1 ml-4">Users</h4>
             <v-spacer></v-spacer>
-            <v-btn dense color="primary" class="mr-4" @click="createUserDialog=true">Create User</v-btn>
+            <v-btn dense color="primary" class="mr-4" @click="openCreateUserDialog()">Create User</v-btn>
           </v-col>
         </v-row>
         <v-divider></v-divider>
         <v-row class="d-flex justify-center">
           <v-col cols="11">
-            <table class="mt-6">
+            <table class="mt-6" id="employeelist">
               <tr>
                 <th>Name</th>
                 <th>SurName</th>
@@ -20,13 +20,13 @@
                 <th>Activate</th>
                 <th>Action</th>
               </tr>
-              <tr v-for="user in users" :key="user.id">
+              <tr v-for="(user,idx) in users" :key="user.id" id="employeelist">
                 <td>{{user.Name}}</td>
                 <td>{{user.Surname}}</td>
                 <td>{{user.Email}}</td>
                 <td>{{user.Phone}}</td>
                 <td>{{user.Status}}</td>
-                <td><v-btn dense color="success">Edit</v-btn><v-btn dense color="error" class="ml-2">Delete</v-btn></td>
+                <td><v-btn dense color="success" @click="editUserDetails(idx)">Edit</v-btn><v-btn dense color="error" class="ml-2" @click="remove(idx)">Delete</v-btn></td>
               </tr>
             </table>
           </v-col>
@@ -38,7 +38,7 @@
                 <v-row>
                   <v-col md="6">
                     <label for="Name">Name</label><br/>
-                    <v-text-field dense outlined v-model="name"></v-text-field>
+                    <v-text-field dense outlined v-model="Name"></v-text-field>
                   </v-col>
                   <v-col md="6">
                     <label for="Surname">Surname</label><br/>
@@ -82,7 +82,67 @@
                 </v-row>
                 <v-row>
                   <v-col cols="3">
-                    <v-btn color="primary" dense @click="createUserDialog=false">Save</v-btn>
+                    <v-btn color="primary" dense @click="createUser()">Save</v-btn>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-dialog>
+        <!-- create user dialog ends -->
+        <!-- edit user dialog starts -->
+        <v-dialog v-model="editUserDialog" width="800" persitent style="overflow-x: hidden">
+          <v-card style="height:100vh" elevation-1>
+            <v-row>
+              <v-col cols="12" class="pa-8">
+                <v-row>
+                  <v-col md="6">
+                    <label for="Name">Name</label><br/>
+                    <v-text-field dense outlined v-model="editUser.Name"></v-text-field>
+                  </v-col>
+                  <v-col md="6">
+                    <label for="Surname">Surname</label><br/>
+                    <v-text-field dense outlined v-model="editUser.surname"></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col md="6">
+                    <label for="Email">Email Address</label><br/>
+                    <v-text-field dense outlined v-model="editUser.email" ></v-text-field>
+                  </v-col>
+                  <v-col md="6">
+                    <label for="Surname">Password</label><br/>
+                    <v-text-field dense outlined v-model="password"></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col md="6">
+                    <label for="username">Username</label><br/>
+                    <v-text-field dense outlined v-model="editUser.username"></v-text-field>
+                  </v-col>
+                  <v-col md="6">
+                    <label for="company">Company</label><br/>
+                    <v-text-field dense outlined v-model="editUser.company"></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col md="6">
+                    <label for="Phone">Phone Number</label><br/>
+                    <v-text-field dense outlined v-model="editUser.phone"></v-text-field>
+                  </v-col>
+                  <v-col md="6">
+                    <label for="Countrycode">Phone Country Code</label><br/>
+                    <v-text-field dense outlined v-model="editUser.countrycode"></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="3">
+                    <button>choose file</button>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="3">
+                    <v-btn color="primary" dense @click="UpdateUser()">Save</v-btn>
                   </v-col>
                 </v-row>
               </v-col>
@@ -102,8 +162,9 @@ export default {
     return{
       users: userData,
       createUserDialog: false,
+      editUserDialog: false,
       // create user form data
-      name: "",
+      Name: "",
       surname: "",
       email: "",
       password: "",
@@ -111,6 +172,19 @@ export default {
       company: "",
       phone: "",
       countrycode: "",
+      //edit details data
+      editUser: {
+        Name: "",
+        surname: "",
+        email: "",
+        password: "",
+        username: "",
+        phone: "",
+        company: "",
+        countrycode: ""
+      },
+      selectedRow: null,
+      
       
     }
   },
@@ -118,6 +192,60 @@ export default {
   components: {
     // HelloWorld,
   },
+
+  methods:{
+    openCreateUserDialog(){
+      this.createUserDialog = true;
+    },
+
+    createUser(){
+      this.users.push({
+        "id": this.users.length,
+        "Name": this.Name,
+        "Surname": this.surname,
+        "Email": this.email,
+        "Phone": this.phone,
+        "Status": "Active"
+      });
+      this.createUserDialog= false;
+    },
+
+    editUserDetails(idx){
+      this.selectedRow = idx;
+      this.editUserDialog= true;
+      this.editUser.Name = this.users[idx].Name;
+      this.editUser.surname = this.users[idx].Surname;
+      this.editUser.email = this.users[idx].Email;
+      this.editUser.password = this.users[idx].Password;
+      this.editUser.username = this.users[idx].Username;
+      this.editUser.company = this.users[idx].Company;
+      this.editUser.phone = this.users[idx].Phone;
+      this.editUser.countrycode = this.users[idx].Countrycode;
+    },
+
+    async UpdateUser(){
+      
+      this.users[this.selectedRow].id = this.selectedRow+1;
+      this.users[this.selectedRow].Name = this.editUser.Name;
+      this.users[this.selectedRow].Surname = this.editUser.surname;
+      this.users[this.selectedRow].Email = this.editUser.email;
+      this.users[this.selectedRow].Password = this.editUser.password;
+      this.users[this.selectedRow].Username = this.editUser.username;
+      this.users[this.selectedRow].Company = this.editUser.company;
+      this.users[this.selectedRow].Countrycode = this.editUser.countrycode;
+      this.users[this.selectedRow].Phone = this.editUser.phone;
+
+      this.editUserDialog= false;
+    },
+
+    remove(idx){
+      if(confirm("Are u sure to delete this userr?")){
+        document.getElementById("employeelist").deleteRow(idx+1)[idx];
+      }
+    }
+    
+   
+  }
 };
 </script>
 
