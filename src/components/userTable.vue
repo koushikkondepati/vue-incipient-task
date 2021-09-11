ca55cbc1e4e4f896382a2f43fa35034841c4432a
koushik k <koushik@billionlives.in>
2021-09-11 20:40:15 +0530

    removed unwanted code

--- src/components/userTable.vue
+++ src/components/userTable.vue
@@ -1,13 +1,5 @@
 <template>
     <div>
-        <!-- <v-row> 
-          <v-col cols="12" class="d-inline-flex" style="height:70px">
-            <h4 class="pt-1 ml-4">Users</h4>
-            <v-spacer></v-spacer>
-            <v-btn dense color="primary" class="mr-4" @click.stop="openCreateUserDialog()">Create User</v-btn>
-          </v-col>
-        </v-row> -->
-        <!-- <v-divider></v-divider> -->
         <v-row  class="d-flex justify-center">
             <v-col cols="11">
                 <div class="v-scrolling-div" style="height:420px">
@@ -20,14 +12,6 @@
                     <th>website</th>
                     <th>Action</th>
                 </tr>
-                <!-- <tr v-for="(user,idx) in data" :key="user.id" id="employeelist">
-                    <td>{{user.name}}</td>
-                    <td>{{user.username}}</td>
-                    <td>{{user.email}}</td>
-                    <td>{{user.phone}}</td>
-                    <td>{{user.website}}</td>
-                    <td><v-btn dense color="success" @click.stop="editUserDetails(idx)">Edit</v-btn><v-btn dense color="error" class="ml-2" @click="remove(idx)">Delete</v-btn></td>
-                </tr> -->
                 <tr v-for="(user) in allUsers" :key="user.id" id="employeelist">
                     <td>{{user.name}}</td>
                     <td>{{user.username}}</td>
@@ -40,6 +24,7 @@
                 </div>
           </v-col>
         </v-row>
+        <!-- userDialog component -->
         <v-row>
           <UserDialog :userDialog="userDialog" :editenabled="editenabled"></UserDialog>
         </v-row>
@@ -57,28 +42,8 @@ export default {
       data: [],
       // users: userData,
       UserDialog: false,
-      // create user form data
-      Name: "",
-      surname: "",
-      email: "",
-      password: "",
-      username: "",
-      website: "",
-      phone: "",
-      countrycode: "",
-      //edit details data
-      editUser: {
-        Name: "",
-        surname: "",
-        email: "",
-        password: "",
-        username: "",
-        phone: "",
-        website: "",
-        countrycode: ""
-      },
       selectedRow: null,
-      usersdata: "",
+      // usersdata: "",
       editenabled: false,
       
       //validation
@@ -98,7 +63,7 @@ export default {
   },
 
   mounted(){
-      // this.getData();
+      
   },
 
   computed: mapGetters(["allUsers","dialogStatus"]),
